4b3cc49913d18a8692edfb4d05e426b588e6a0d4
Peter McNeely <mcneelypj@msoe.edu>
2017-04-25 17:29:00 -0500

    added constructor and refresh methods

--- test/StockQuoteAnalyzerTest.java
+++ test/StockQuoteAnalyzerTest.java
@@ -45,6 +45,40 @@ public class StockQuoteAnalyzerTest {
     }
 
     @Test
+    public void constructorShouldInitalizeCorrectlyWhenSymbolIsValid() throws Exception {
+        analyzer = new StockQuoteAnalyzer("HOG", generatorMock, audioMock);
+    }
+
+    @Test (expectedExceptions = NullPointerException.class)
+    public void constructorShouldThrowExceptionWhenAudioIsNull() throws Exception {
+        analyzer = new StockQuoteAnalyzer("HOG", null, audioMock);
+    }
+
+
+    @Test
+    public void refreshShouldRefreshStockPriceWhenCalled() throws Exception {
+        analyzer = new StockQuoteAnalyzer("HOG", generatorMock, audioMock);
+
+        double prevClose = 56.57;
+        double lastTrade = 57.21;
+        double change = .89;
+
+        StockQuoteInterface newValue = new StockQuote("HOG", prevClose, lastTrade, change);
+        when(generatorMock.getCurrentQuote()).thenReturn(newValue);
+
+        analyzer.refresh();
+        verify(generatorMock, times(1)).getCurrentQuote();
+    }
+
+    @Test (expectedExceptions = StockTickerConnectionError.class)
+    public void refreshShouldThrowExceptionWhenProblemOccurred() throws Exception {
+        analyzer = new StockQuoteAnalyzer("HOG", generatorMock, audioMock);
+
+        when(generatorMock.getCurrentQuote()).thenThrow(Exception.class);
+        analyzer.refresh();
+    }
+
+    @Test
     public void getSymbolShouldReturnAppropriateSymbol() throws Exception {
         // Arrange
         analyzer = new StockQuoteAnalyzer("CBD", generatorMock, audioMock);
